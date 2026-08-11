f8fe6e6b414294500f2c1b5787967e05434b9dcf
syifaaazkaa- <syifa.azka1212@sma.belajar.id>
2026-08-11 13:00:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-// Smooth Animation
+const buttons = document.querySelectorAll(".btn");
 
-const cards = document.querySelectorAll(".card");
-
-window.addEventListener("scroll", ()=>{
-
-cards.forEach(card=>{
-
-const top = card.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-
-card.style.opacity="1";
-card.style.transform="translateY(0px)";
-
-}
-
-});
-
-});
-
-cards.forEach(card=>{
-
-card.style.opacity="0";
-card.style.transform="translateY(50px)";
-card.style.transition="0.6s";
-
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (button.getAttribute("href") === "#") {
+      event.preventDefault();
+      alert("Link bisa kamu ganti dengan URL GitHub atau project kamu.");
+    }
+  });
 });
